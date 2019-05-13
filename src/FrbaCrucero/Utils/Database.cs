2dc117b06eff81baa736474237d9d5c952df735c
Muabid <bruno2698@gmail.com>
2019-05-12 21:04:39 -0300

    Agrego interfaz para facilitar la conexión a la db

---
+++ src/FrbaCrucero/Utils/Database.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrbaCrucero.Utils
+{
+    class Database
+    {
+        private static String connectionStr = ConfigurationManager.ConnectionStrings["FRBACrucero"]
+            .ConnectionString;
+        private static SqlConnection connection = new SqlConnection(connectionStr);
+
+        public static SqlConnection getConnection()
+        {
+            return connection;
+        }
+
+        public static SqlCommand createCommand(String query)
+        {
+            return new SqlCommand(query,connection);
+        }
+
+        public static int execute(SqlCommand command)
+        {
+            
+            connection.Open();
+            int res = 0;
+            try
+            {
+                res = command.ExecuteNonQuery();
+            }
+            catch (Exception exception)
+            { 
+            }
+            connection.Close();
+            return res;
+        }
+
+        public static int executeProcedure(SqlCommand command)
+        {
+            command.CommandType = CommandType.StoredProcedure;
+            return execute(command);
+        }
+
+        public static DataTable getData(SqlCommand query)
+        {
+           DataTable data = new DataTable();
+           
+            try{
+                SqlDataAdapter adapter = new SqlDataAdapter(query);
+                adapter.Fill(data);
+            }
+            catch(Exception exception){
+                Console.WriteLine(exception.Message);
+            }
+            return data;
+        }
+        
+
+
+
+    }
+}
---
+++ src/FrbaCrucero/Utils/RecorridoDAO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace FrbaCrucero.Utils
+{
+    class RecorridoDAO
+    {
+
+        public DataTable getRecorridos()
+        {
+            SqlCommand query = Database.createCommand("SELECT reco_codigo FROM MACACO_NOT_NULL.RECORRIDO");
+            return Database.getData(query);
+        }
+    }
+}
