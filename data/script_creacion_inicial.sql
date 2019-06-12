1bac08d6e9dfeda1735287e5c495e7cf14a9ae11
german <germanjugo@hotmail.com>
2019-06-12 11:37:53 -0300

    subo nueva migracion

--- data/script_creacion_inicial.sql
+++ data/script_creacion_inicial.sql
@@ -1,5 +1,6 @@
 USE [GD1C2019]
 
+
 GO
 CREATE FUNCTION [MACACO_NOT_NULL].EncriptarPassword(@password NVARCHAR(255))
 RETURNS nvarchar(255)
@@ -123,23 +124,9 @@ CREATE TABLE [MACACO_NOT_NULL].[COMPANIA] (
 END
 GO
 
-IF NOT EXISTS (
-	SELECT 1
-	FROM INFORMATION_SCHEMA.TABLES 
-	WHERE TABLE_TYPE = 'BASE TABLE'
-	AND TABLE_NAME = 'RESERVA'
-	AND TABLE_SCHEMA = 'MACACO_NOT_NULL'
-)
-BEGIN
-CREATE TABLE [MACACO_NOT_NULL].[RESERVA] (
-	rese_id  int IDENTITY(1,1) PRIMARY KEY NOT NULL, 
-	rese_usuario_id int FOREIGN KEY REFERENCES [MACACO_NOT_NULL].[USUARIO] (usua_id),
-	rese_codigo decimal (18,0) NOT NULL,
-	rese_fecha datetime2 (3) NOT NULL,
-	rese_viaje_id int FOREIGN KEY REFERENCES [MACACO_NOT_NULL].[VIAJE] (viaj_id)
-);
-END
-GO
+
+
+
 
 IF NOT EXISTS (
 	SELECT 1
@@ -294,6 +281,27 @@ BEGIN
  );
  END
  GO
+
+
+
+ IF NOT EXISTS (
+	SELECT 1
+	FROM INFORMATION_SCHEMA.TABLES 
+	WHERE TABLE_TYPE = 'BASE TABLE'
+	AND TABLE_NAME = 'RESERVA'
+	AND TABLE_SCHEMA = 'MACACO_NOT_NULL'
+)
+BEGIN
+CREATE TABLE [MACACO_NOT_NULL].[RESERVA] (
+	rese_id  int IDENTITY(1,1) PRIMARY KEY NOT NULL, 
+	rese_usuario_id int FOREIGN KEY REFERENCES [MACACO_NOT_NULL].[USUARIO] (usua_id),
+	rese_codigo decimal (18,0) NOT NULL,
+	rese_fecha datetime2 (3) NOT NULL,
+	rese_viaje_id int FOREIGN KEY REFERENCES [MACACO_NOT_NULL].[VIAJE] (viaj_id)
+);
+END
+GO
+
 
  IF NOT EXISTS (
 	SELECT 1
@@ -1488,3 +1496,130 @@ DROP TABLE [MACACO_NOT_NULL].[ROL]
 DROP SEQUENCE [MACACO_NOT_NULL].CountBy1 
 
 DROP SCHEMA MACACO_NOT_NULL */
+
+
+
+/********************************************************
+ COPYRIGHTS http://www.ranjithk.com
+*********************************************************/  
+CREATE PROCEDURE CleanUpSchema
+(
+  @SchemaName varchar(100)
+ ,@WorkTest char(1) = 'w'  -- use 'w' to work and 't' to print
+)
+AS
+/*-----------------------------------------------------------------------------------------
+  
+  Author : Ranjith Kumar S
+  Date:    31/01/10
+  
+  Description: It drop all the objects in a schema and then the schema itself
+  
+  Limitations:
+   
+    1. If a table has a PK with XML or a Spatial Index then it wont work 
+       (workaround: drop that table manually and re run it)
+    2. If the schema is referred by a XML Schema collection then it wont work
+
+If it is helpful, Please send your comments ranjith_842@hotmail.com or visit http://www.ranjithk.com
+ 
+-------------------------------------------------------------------------------------------*/
+BEGIN    
+
+declare @SQL varchar(4000)
+declare @msg varchar(500)
+ 
+IF OBJECT_ID('tempdb..#dropcode') IS NOT NULL DROP TABLE #dropcode
+CREATE TABLE #dropcode
+(
+   ID int identity(1,1)
+  ,SQLstatement varchar(1000) 
+ )
+
+-- removes all the foreign keys that reference a PK in the target schema
+ SELECT @SQL = 
+  'select 
+       '' ALTER TABLE ''+SCHEMA_NAME(fk.schema_id)+''.''+OBJECT_NAME(fk.parent_object_id)+'' DROP CONSTRAINT ''+ fk.name
+  FROM sys.foreign_keys fk
+  join sys.tables t on t.object_id = fk.referenced_object_id
+  where t.schema_id = schema_id(''' + @SchemaName+''')
+    and fk.schema_id <> t.schema_id 
+  order by fk.name desc'
+ 
+ IF @WorkTest = 't' PRINT (@SQL )
+ INSERT INTO #dropcode
+ EXEC (@SQL)
+   
+ -- drop all default constraints, check constraints and Foreign Keys
+ SELECT @SQL = 
+ 'SELECT 
+       '' ALTER TABLE ''+schema_name(t.schema_id)+''.''+OBJECT_NAME(fk.parent_object_id)+'' DROP CONSTRAINT ''+ fk.[Name]
+  FROM sys.objects fk
+  join sys.tables t on t.object_id = fk.parent_object_id
+  where t.schema_id = schema_id(''' + @SchemaName+''')
+   and fk.type IN (''D'', ''C'', ''F'')'
+   
+ IF @WorkTest = 't' PRINT (@SQL )
+ INSERT INTO #dropcode
+ EXEC (@SQL)
+  
+ -- drop all other objects in order    
+ SELECT @SQL =   
+ 'SELECT 
+      CASE WHEN SO.type=''PK'' THEN '' ALTER TABLE ''+SCHEMA_NAME(SO.schema_id)+''.''+OBJECT_NAME(SO.parent_object_id)+'' DROP CONSTRAINT ''+ SO.name
+           WHEN SO.type=''U'' THEN '' DROP TABLE ''+SCHEMA_NAME(SO.schema_id)+''.''+ SO.[Name]
+           WHEN SO.type=''V'' THEN '' DROP VIEW  ''+SCHEMA_NAME(SO.schema_id)+''.''+ SO.[Name]
+           WHEN SO.type=''P'' THEN '' DROP PROCEDURE  ''+SCHEMA_NAME(SO.schema_id)+''.''+ SO.[Name]          
+           WHEN SO.type=''TR'' THEN ''  DROP TRIGGER  ''+SCHEMA_NAME(SO.schema_id)+''.''+ SO.[Name]
+           WHEN SO.type  IN (''FN'', ''TF'',''IF'',''FS'',''FT'') THEN '' DROP FUNCTION  ''+SCHEMA_NAME(SO.schema_id)+''.''+ SO.[Name]
+       END
+FROM sys.objects SO
+WHERE SO.schema_id = schema_id('''+ @SchemaName +''')
+  AND SO.type IN (''PK'', ''FN'', ''TF'', ''TR'', ''V'', ''U'', ''P'')
+ORDER BY CASE WHEN type = ''PK'' THEN 1 
+              WHEN type in (''FN'', ''TF'', ''P'',''IF'',''FS'',''FT'') THEN 2
+              WHEN type = ''TR'' THEN 3
+              WHEN type = ''V'' THEN 4
+              WHEN type = ''U'' THEN 5
+            ELSE 6 
+          END'
+
+IF @WorkTest = 't' PRINT (@SQL )
+INSERT INTO #dropcode
+EXEC (@SQL)
+  
+DECLARE @ID int, @statement varchar(1000)
+DECLARE statement_cursor CURSOR
+FOR SELECT SQLstatement
+      FROM #dropcode
+  ORDER BY ID ASC
+     
+ OPEN statement_cursor
+ FETCH statement_cursor INTO @statement 
+ WHILE (@@FETCH_STATUS = 0)
+ BEGIN
+ 
+ IF @WorkTest = 't' PRINT (@statement)
+ ELSE
+  BEGIN
+    PRINT (@statement)
+    EXEC(@statement) 
+  END
+   
+ FETCH statement_cursor INTO @statement     
+END
+  
+CLOSE statement_cursor
+DEALLOCATE statement_cursor
+  
+IF @WorkTest = 't' PRINT ('DROP SCHEMA '+@SchemaName)
+ELSE
+ BEGIN
+   PRINT ('DROP SCHEMA '+@SchemaName)
+   EXEC ('DROP SCHEMA '+@SchemaName)
+ END  
+ 
+PRINT '------- ALL - DONE -------'    
+END
+
+--exec dbo.CleanUpSchema 'MACACO_NOT_NULL'  EJEMPLO PARA BORRAR TODO
