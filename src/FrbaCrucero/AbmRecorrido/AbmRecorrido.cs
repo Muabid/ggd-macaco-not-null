8fa8c5d0d71dd6e232d71ba5d9dc74c6e61f36cf
Bruno Cobos <bruno2698@gmail.com>
2019-06-12 22:48:49 -0300

    Abm Recorrido

---
+++ src/FrbaCrucero/AbmRecorrido/AbmRecorrido.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrbaCrucero.AbmRecorrido
+{
+    public partial class AbmRecorrido : Form
+    {
+        public AbmRecorrido()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAlta_Click(object sender, EventArgs e)
+        {
+            BajaRecorrido baja = new BajaRecorrido();
+            baja.MdiParent = this;
+            baja.Show();
+        }
+
+        private void btnBaja_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnModificacion_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
