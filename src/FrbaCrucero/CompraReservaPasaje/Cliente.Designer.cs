66d192d1261841bd2f55962c1683dbd6dbe95631
german <germanjugo@hotmail.com>
2019-06-23 15:59:58 -0300

    mas cambios

--- src/FrbaCrucero/CompraReservaPasaje/Cliente.Designer.cs
+++ src/FrbaCrucero/CompraReservaPasaje/Cliente.Designer.cs
@@ -45,6 +45,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.btn_atras = new System.Windows.Forms.Button();
             this.btn_limpiar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -226,11 +228,31 @@
             this.btn_limpiar.UseVisualStyleBackColor = true;
             this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(484, 297);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Reservar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(379, 297);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Pagar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Cliente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(595, 330);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btn_atras);
             this.Controls.Add(this.btn_limpiar);
             this.Controls.Add(this.groupBox1);
@@ -262,5 +284,7 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Button btn_atras;
         private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
---
+++ src/FrbaCrucero/CompraReservaPasaje/Pago.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace FrbaCrucero.CompraReservaPasaje
+{
+    partial class Pago
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_atras = new System.Windows.Forms.Button();
+            this.botonBuscarReserva = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pagoReservaTable = new System.Windows.Forms.DataGridView();
+            this.codigoReservaBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pagoReservaTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_atras
+            // 
+            this.btn_atras.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_atras.Location = new System.Drawing.Point(57, 285);
+            this.btn_atras.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_atras.Name = "btn_atras";
+            this.btn_atras.Size = new System.Drawing.Size(73, 25);
+            this.btn_atras.TabIndex = 29;
+            this.btn_atras.Text = "Atrás";
+            this.btn_atras.UseVisualStyleBackColor = true;
+            // 
+            // botonBuscarReserva
+            // 
+            this.botonBuscarReserva.Location = new System.Drawing.Point(515, 37);
+            this.botonBuscarReserva.Name = "botonBuscarReserva";
+            this.botonBuscarReserva.Size = new System.Drawing.Size(75, 23);
+            this.botonBuscarReserva.TabIndex = 28;
+            this.botonBuscarReserva.Text = "Buscar";
+            this.botonBuscarReserva.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(375, 288);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 27;
+            this.button2.Text = "Limpiar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(515, 288);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 26;
+            this.button1.Text = "Pagar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pagoReservaTable
+            // 
+            this.pagoReservaTable.AllowUserToAddRows = false;
+            this.pagoReservaTable.AllowUserToDeleteRows = false;
+            this.pagoReservaTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.pagoReservaTable.Location = new System.Drawing.Point(57, 82);
+            this.pagoReservaTable.Name = "pagoReservaTable";
+            this.pagoReservaTable.ReadOnly = true;
+            this.pagoReservaTable.Size = new System.Drawing.Size(533, 199);
+            this.pagoReservaTable.TabIndex = 25;
+            // 
+            // codigoReservaBox
+            // 
+            this.codigoReservaBox.Location = new System.Drawing.Point(336, 39);
+            this.codigoReservaBox.Name = "codigoReservaBox";
+            this.codigoReservaBox.Size = new System.Drawing.Size(161, 20);
+            this.codigoReservaBox.TabIndex = 24;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
+            this.label1.Location = new System.Drawing.Point(51, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 22);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Detalles de la compra";
+            // 
+            // PagoDeLaReserva
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 415);
+            this.Controls.Add(this.btn_atras);
+            this.Controls.Add(this.botonBuscarReserva);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pagoReservaTable);
+            this.Controls.Add(this.codigoReservaBox);
+            this.Controls.Add(this.label1);
+            this.Name = "PagoDeLaReserva";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.pagoReservaTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_atras;
+        private System.Windows.Forms.Button botonBuscarReserva;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView pagoReservaTable;
+        private System.Windows.Forms.TextBox codigoReservaBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
